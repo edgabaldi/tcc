55db1e0d6d3c767b905db623bef054ba5b62aeeb
Edgar Gabaldi <edgabaldi@gmail.com>
2015-08-28 22:33:07 -0300

    adicionando página do produto

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import include, url, patterns
 urlpatterns = patterns('',
     url(regex=r'^$',
         view = 'website.views.index',
-        name = 'index'),
+        name = 'index'
+    ),
+    url(regex=r'^product$',
+        view = 'website.views.product',
+        name = 'product',
+    ),
 )
 
 
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response('website/index.html')
+
+def product(request):
+    return render_to_response('website/product.html')
