bec9b7a22a8f25d3d87b89b9cd85bb85bb333766
Edgar Gabaldi <edgabaldi@gmail.com>
2015-08-30 17:23:20 -0300

    fix menu active options

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 
 def customer_list(request):
-    return render_to_response('customer/customer_list.html')
+    return render_to_response('customer/customer_list.html', context = {
+        'menu_active':'customer'})
 
 def customer_form(request):
-    return render_to_response('customer/customer_form.html')
+    return render_to_response('customer/customer_form.html', context = {
+        'menu_active':'customer'})
--- product/views.py
+++ product/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render_to_response
 
 
 def product_list(request):
-    return render_to_response('product/product_list.html')
+    return render_to_response('product/product_list.html', context = {
+        'menu_active': 'product'})
 
 def product_form(request):
-    return render_to_response('product/product_form.html')
+    return render_to_response('product/product_form.html', context={
+        'menu_active': 'product'})
