52a2a9d7122f9044240d30fb2c4a7b1b776626d4
Edgar Gabaldi <edgabaldi@gmail.com>
2015-09-08 20:33:16 -0300

    remove critics example from recommender management command

--- product/management/commands/recommender.py
+++ product/management/commands/recommender.py
@@ -6,51 +6,6 @@ import pandas as pd
 
 import pickle
 
-critics={
-    'Lisa Rose': {
-        'Lady in the Water': 2.5, 
-        'Snakes on a Plane': 3.5,
-        'Just My Luck': 3.0, 
-        'Superman Returns': 3.5, 
-        'You, Me and Dupree': 2.5, 
-        'The Night Listener': 3.0},
-    'Gene Seymour': {
-        'Lady in the Water': 3.0, 
-        'Snakes on a Plane': 3.5, 
-        'Just My Luck': 1.5, 
-        'Superman Returns': 5.0, 
-        'The Night Listener': 3.0, 
-        'You, Me and Dupree': 3.5}, 
-    'Michael Phillips': {
-        'Lady in the Water': 2.5, 
-        'Snakes on a Plane': 3.0,
-        'Superman Returns': 3.5, 
-        'The Night Listener': 4.0},
-    'Claudia Puig': {
-        'Snakes on a Plane': 3.5, 
-        'Just My Luck': 3.0,
-         'The Night Listener': 4.5, 
-        'Superman Returns': 4.0, 
-        'You, Me and Dupree': 2.5},
-    'Mick LaSalle': {
-        'Lady in the Water': 3.0, 
-        'Snakes on a Plane': 4.0, 
-        'Just My Luck': 2.0, 
-        'Superman Returns': 3.0, 
-        'The Night Listener': 3.0,
-        'You, Me and Dupree': 2.0}, 
-    'Jack Matthews': {
-        'Lady in the Water': 3.0, 
-        'Snakes on a Plane': 4.0,
-        'The Night Listener': 3.0, 
-        'Superman Returns': 5.0, 
-        'You, Me and Dupree': 3.5},
-    'Toby': {
-        'Snakes on a Plane':4.5,
-        'You, Me and Dupree':1.0,
-        'Superman Returns':4.0}
-}
-
 
 def sim_pearson(preferences, p1, p2):
     """
@@ -90,7 +45,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """
-
         self.dataset = self._load_csv()
 
         user_prefs = self._fill_products()
@@ -106,9 +60,6 @@ class Command(BaseCommand):
         self.dataset = self._load_csv()
 
         user_prefs = self._fill_products()
-
-
-
 
     def _load_csv(self):
         return pd.read_csv('tmp/lances-sem-deposito.csv', sep=';',
