875c4828fb3939f6fad1331731fec18edd60018a
Edgar Gabaldi <edgabaldi@gmail.com>
2015-10-18 10:48:07 -0200

    add initial website tests

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include, url, patterns
 
 urlpatterns = patterns('',
     url(regex=r'^$',
-        view = 'customer.views.customer_list',
+        view = 'account.views.customer_list',
         name = 'customer_list',
     ),
     url(regex=r'^add/$',
-        view = 'customer.views.customer_form',
+        view = 'account.views.customer_form',
         name = 'customer_form',
     ),
 
--- tcc/urls.py
+++ tcc/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('website.urls')),
-    url(r'^customer/', include('customer.urls')),
+    url(r'^account/', include('account.urls')),
     url(r'^product/', include('product.urls')),
 ]
