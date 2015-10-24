e5c1e8d4dddc7a4a51dd1c7d9e16098d633d9d77
Edgar Gabaldi <edgabaldi@gmail.com>
2015-10-24 20:56:09 -0200

    management command that import brbid data

--- tcc/management/commands/importer.py
+++ tcc/management/commands/importer.py
@@ -1,6 +1,9 @@
+#coding:utf-8
+from decimal import Decimal
 from copy import copy
 import datetime
 import pickle
+import pprint
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
@@ -14,29 +17,7 @@ now = datetime.datetime(2015, 10, 16, 12 ,00)
 
 class Command(BaseCommand):
 
-    def _bid_kwargs(self, product, user, bid_dict):
-
-        date = bid_dict['created_at']
-        date = date.replace(tzinfo=None)
-        
-        return {
-            'product': product,
-            'user': user,
-            'value': bid_dict['value'],
-            'created_at': date,
-        }
-
-    def _user_kwargs(self, bid_dict):
-
-        bid_dict = copy(bid_dict)
-
-        del bid_dict['value']
-        del bid_dict['created_at']
-
-        if bid_dict['birth_date'] is None:
-            bid_dict['birth_date'] =  now.date().replace(year=1986)
-
-        return bid_dict
+    photo_list = []
 
     def _vehicle_kwargs(self, each_dict):
 
@@ -51,36 +32,86 @@ class Command(BaseCommand):
             name=each_dict['model_name'],
             is_active=True)
 
-        for key in ['photos', 'bids', 'brand_name', 'model_name']:
+        for key in ['veiculo_id', 'photos', 'bids', 'brand_name', 'model_name']:
             del each_dict[key]
 
         extra = {
             'model': model,
-            'description' : 'xxx',
             'clock_starts_at': now,
             'status': 'em_loteamento'
         }
+
+        if each_dict.get('initial_price') is None:
+
+            extra.update({
+                'initial_price': Decimal('0'),
+            })
 
         each_dict.update(extra)
 
         return each_dict
 
-    def handle(self, *args, **options):
+    def _bid_kwargs(self, product, user, bid_dict):
 
-        with open('tmp/data/data.pkl') as file:
+        date = bid_dict['created_at']
+        date = date.replace(tzinfo=None)
+        
+        return {
+            'product': product,
+            'user': user,
+            'value': bid_dict['value'] or Decimal('0'),
+            'created_at': date,
+        }
+
+    def _user_kwargs(self, user_dict):
+
+        user_dict = copy(user_dict)
+
+        if user_dict['birth_date'] is None:
+            user_dict['birth_date'] =  now.date().replace(year=1986)
+
+        return user_dict
+
+    def _save_photos_to_download(self, id, photo):
+        path_list = photo.split('/')
+        url ="https://brbid-assets-prod.s3.amazonaws.com/media/fotos/" + path_list[-1]
+
+        self.photo_list.append({
+            'id': id, 
+            'url': url,
+        })
+
+    def handle(self, *args, **options):
+        """
+        * salvar pk do veiculo e endereço de download.
+        """
+
+        with open('data.pkl') as file:
 
             self.data = pickle.load(file)
 
         for each in self.data:
-
             vehicle_dict = self._vehicle_kwargs(each)
-
             product, created = Product.objects.get_or_create(**vehicle_dict)
 
+            for photo in each.get('photos'):
+                self._save_photos_to_download(product.pk, photo)
+
+            bid_list = []
             for bid in each.get('bids'):
-                user_dict = self._user_kwargs(bid)
-                user, created = User.objects.get_or_create(**user_dict)
 
-                bid_dict = self._bid_kwargs(product, user, bid)
-                Bid.objects.get_or_create(**bid_dict)
+                try:
 
+                    user = bid.get('user')
+                    user_dict = self._user_kwargs(user)
+                    user, created = User.objects.get_or_create(**user_dict)
+
+                    bid_kwargs = self._bid_kwargs(product, user, bid)
+                    bid_list.append(Bid(**bid_kwargs))
+                except Exception, e:
+                    print user, str(e)
+
+            Bid.objects.bulk_create(bid_list)
+
+        with open('photos.pkl', 'w') as arquivo:
+            data = pickle.dump(self.photo_list, arquivo)
---
+++ tcc/management/commands/photos.py
@@ -0,0 +1,45 @@
+import pickle
+import requests
+from StringIO import StringIO
+
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+
+from product.models import Product, Photo
+
+
+class Command(BaseCommand):
+
+    def _download(self, url):
+        response = requests.get(url)
+
+        file = StringIO(response.content)
+        file_content = ContentFile(file.read())
+        return file_content
+
+    def _get_filename(self, url):
+        return url.split('/')[-1]
+
+    def _get_product(self, id):
+        return Product.objects.get(id=id)
+ 
+    def handle(self, *args, **options):
+
+
+        with open('photo.pkl') as file:
+
+            self.data = pickle.load(file)
+
+        for each in self.data:
+
+            id = each.get('id')
+            url = each.get('url')
+
+            product = self._get_product(id)
+            filename = self._get_filename(url)
+
+            file_content = self._download(url)
+
+            photo = Photo(product=product)
+            photo.file.save(filename, file_content)
+            photo.save()
