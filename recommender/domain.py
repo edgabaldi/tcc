efaed011aff85ec487486c16eee75690ee3d0559
Edgar Gabaldi <edgabaldi@gmail.com>
2015-11-01 12:40:30 -0200

    refactory of recommendation engine

--- recommender/domain.py
+++ recommender/domain.py
@@ -1,80 +1,86 @@
+#coding:utf-8
 import pandas as pd
 
-from recommender.utils import sim_pearson
 from recommender.models import ProductSimilarity
 from recommender.cache import SQLiteCacheBackend
 from product.models import Product, Bid
 
 
-class Recommender(object):
-    """
-    Find the similarities based on preferences.
-
-    Usage:
-        The recommender class wait a list of dictonaries with pattern:
-
-    Data Example:
-        [{'user__id': 123, 'product__reference': 'brand+model+general-state'}]
-    """
+class Model(object):
 
     def __init__(self, queryset):
 
         self.cache = SQLiteCacheBackend()
-        self.dataset = pd.DataFrame(list(queryset))
+        self.data = pd.DataFrame(list(queryset))
 
-    def top_matches(self, preferences, entity, n=8, similarity=sim_pearson):
-        """
-        Returns the best matches for entity from the preferences dictionary.
-        """
+    def create(self):
 
-        scores=[(similarity(preferences, entity, other), other) 
-            for other in preferences if other!=entity]
-        scores.sort()
-        scores.reverse()
-        return scores[0:n]
+        prefs = self._fill_products()
+        self.cache['prefs'] = prefs
 
-    def initialize_user_dict(self):
+    def get_preferences(self):
+        return self.cache['prefs']
+
+    def _initialize_user_dict(self):
+
         di = {}
-        for user in self.dataset[self._unique_users_mask()].user__id:
+        for user in self.data[self._unique_users_mask()].user__id:
             di[user] = {}
         return di
 
-    def fill_products(self):
+    def _fill_products(self):
 
-        user_dict = self.initialize_user_dict()
+        user_dict = self._initialize_user_dict()
 
         for user in user_dict.keys():
 
             for product in self._all_products():
-                user_dict[user][product] = 0.0
+                user_dict[user][product] = 0
 
             for product in self._products_of_user(user):
-                user_dict[user][product] = 1.0
+                user_dict[user][product] = 1
 
-        return user_dict
-
-    def transform_preferences(self, preferences):
-        result={}
-        for entity in preferences:
-            for item in preferences[entity]:
-                result.setdefault(item,{})
-
-                # Flip item and entity
-                result[item][entity]=preferences[entity][item]
-
-        return result
+        return pd.DataFrame(user_dict)
 
     def _unique_users_mask(self):
-        return (self.dataset.user__id.duplicated()==False)
+        return (self.data.user__id.duplicated()==False)
 
     def _unique_products_mask(self):
-        return (self.dataset.product__reference.duplicated()==False)
+        return (self.data.product__reference.duplicated()==False)
 
     def _products_of_user(self, user):
-        return set(self.dataset[self.dataset.user__id == user].product__reference)
+        return set(self.data[self.data.user__id == user].product__reference)
 
     def _all_products(self):
-        return self.dataset[self._unique_products_mask()].product__reference
+        return self.data[self._unique_products_mask()].product__reference
+
+
+class Recommender(object):
+    """
+    Find the similarities based on preferences.
+    """
+
+    def __init__(self, preferences):
+
+        self.preferences = preferences
+
+    def find_similars_to(self, entity, n=8, transpose=False):
+
+        df = self.preferences.transpose() if transpose else self.preferences
+
+        _pcorr = lambda i, j: df.ix[[i, j]].transpose().corr().fillna(0, inplace=False).loc[i, j]
+
+        def _inner(i,j):
+            try:
+                return _pcorr(i, j)
+            except:
+                raise Exception(u", ".join([str(i), str(j)]))
+
+        matches = [(_inner(entity, other), other) for other in df.index if entity != other]
+
+        similar = [(score, other) for (score, other) in matches if score > 0]
+
+        return sorted(similar, reverse=True)[:n]
 
 
 class RecommendationCommand(object):
@@ -82,34 +88,42 @@ class RecommendationCommand(object):
     @staticmethod
     def run():
 
-        queryset = Bid.objects.values('user__id', 'product__reference')
+        # train model
+        queryset = Bid.objects.values('user__id', 'product__reference').distinct()
+        model = Model(queryset)
+        model.create()
 
-        cmd = Recommender(queryset)
-        cmd.cache['user_prefs'] = cmd.fill_products()
-        cmd.cache['item_prefs'] = cmd.transform_preferences(cmd.cache['user_prefs'])
+        # prepare recommendation
+        recommender = Recommender(model.get_preferences())
 
-        product_list = Product.objects.filter(status='liberado_lance')
+        product_list = Product.objects.filter(status='liberado_lance').distinct()
 
         for product in product_list:
 
             print product.reference
 
-            recommendation = cmd.top_matches(cmd.cache['item_prefs'], product.reference)
+            # find similar products
+            similars = recommender.find_similars_to(product.reference)
 
-            for score, other_reference in recommendation:
-                print product.reference + ' -> ' + other_reference
+            # save similar products
+            instance_list = []
+            for score, other_reference in similars:
 
-                other_list = product_list.filter(reference=other_reference)
+                if product.reference != other_reference:
 
-                if len(other_list) > 0:
+                    other_products = product_list.filter(
+                        similars=None,
+                        reference=other_reference)
 
-                    other_product = other_list[0]
+                    if len(other_products) > 0:
 
-                    assert product != other_product
+                        other_product = other_products[0]
 
-                    similar = ProductSimilarity(
-                        score=score,
-                        product=product,
-                        is_similar_to=other_product)
+                        print product.reference + ' -> ' + other_reference
 
-                    similar.save()
+                        instance_list.append(ProductSimilarity(
+                            score=score,
+                            product=product,
+                            is_similar_to=other_product))
+
+            ProductSimilarity.objects.bulk_create(instance_list)
