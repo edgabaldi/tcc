40ac6b2726fb1ecc6fb8194380d8768c9b9ca45c
Edgar Gabaldi <edgabaldi@gmail.com>
2015-11-21 18:17:04 -0200

    add active menu in user recommendation view

--- recommender/views.py
+++ recommender/views.py
@@ -1,12 +1,15 @@
 #coding:utf-8
-from core.views import SearchableListView, LoginRequiredMixin 
+from core.views import SearchableListView, LoginRequiredMixin, MenuActiveMixin
 from recommender.models import UserSimilarity
 from recommender.forms import UserSimilaritySearchForm
 
 
-class UserRecommendationView(LoginRequiredMixin, SearchableListView):
+class UserRecommendationView(MenuActiveMixin, LoginRequiredMixin,
+                             SearchableListView):
 
     template_name = 'recommender/user_recommendation.html'
     model = UserSimilarity
     paginate_by=25
     form_class = UserSimilaritySearchForm
+    menu_active = 'dashboard'
+    submenu_active = 'user_recommendation'
